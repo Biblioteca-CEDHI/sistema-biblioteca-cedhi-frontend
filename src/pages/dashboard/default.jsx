631363486618ce9bc06257a8452b44bb0c35c6f3
AngelesVasquezU <maria.vasquez.pineda@ucsp.edu.pe>
2025-10-23 20:25:19 -0500

    correcion lineas raras

--- src/pages/dashboard/default.jsx
+++ src/pages/dashboard/default.jsx
@@ -4,25 +4,26 @@ import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
 
 // project-imports
+import useAuth from '../../hooks/useAuth';
 import ReportCard from '../../components/cards/statistics/ReportCard';
 import RankingUsers from '../../sections/widget/data/RankingUsers';
 import RankingBooks from '../../sections/widget/data/RankingBooks';
 import { useGetTotalBooksUsers } from '../../api/stadistic';
 
 // assets
-import { Book, Profile2User} from 'iconsax-react';
+import { Book, Profile2User } from 'iconsax-react';
 import WelcomeBanner from '../../sections/dashboard/WelcomeBanner';
 
 // ==============================|| DASHBOARD - DEFAULT ||============================== //
 
 export default function DashboardDefault() {
   const theme = useTheme();
-
+  const { user } = useAuth();
   const { totalBooksUsersLoading, totalBooksUsers: list } = useGetTotalBooksUsers();
 
   return (
     <Grid container rowSpacing={4.5} columnSpacing={2.75}>
-      {/* row 1 */}
+
       <Grid item xs={12} sm={12} md={9} lg={9}>
         <WelcomeBanner />
       </Grid>
@@ -36,75 +37,16 @@ export default function DashboardDefault() {
           </Grid>
         </Grid>
       </Grid>
-
-      {/* row 2 */}
-      {/* <Grid item xs={12} sm={6} lg={3}>
-        <EcommerceDataCard
-          title="Total de préstamos"
-          count="3000"
-          iconPrimary={<Wallet3 />}
-          percentage={
-            <Typography color="primary" sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
-              <ArrowUp size={16} style={{ transform: 'rotate(45deg)' }} /> 30.6%
-            </Typography>
-          }
-        >
-          <EcommerceDataChart color={theme.palette.primary.main} />
-        </EcommerceDataCard>
-      </Grid>
-      <Grid item xs={12} sm={6} lg={3}>
-        <EcommerceDataCard
-          title="Prestamos activos"
-          count="290"
-          color="warning"
-          iconPrimary={<Book color={theme.palette.warning.dark} />}
-          percentage={
-            <Typography color="warning.dark" sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
-              <ArrowDown size={16} style={{ transform: 'rotate(-45deg)' }} /> 30.6%
-            </Typography>
-          }
-        >
-          <EcommerceDataChart color={theme.palette.warning.dark} />
-        </EcommerceDataCard>
-      </Grid>
-      <Grid item xs={12} sm={6} lg={3}>
-        <EcommerceDataCard
-          title="Disponibilidad de libros"
-          count="1568"
-          color="success"
-          iconPrimary={<Calendar color={theme.palette.success.darker} />}
-          percentage={
-            <Typography color="success.darker" sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
-              <ArrowUp size={16} style={{ transform: 'rotate(45deg)' }} /> 30.6%
-            </Typography>
-          }
-        >
-          <EcommerceDataChart color={theme.palette.success.darker} />
-        </EcommerceDataCard>
-      </Grid>
-      <Grid item xs={12} sm={6} lg={3}>
-        <EcommerceDataCard
-          title="Sanciones por retrasos"
-          count="200"
-          color="error"
-          iconPrimary={<CloudChange color={theme.palette.error.dark} />}
-          percentage={
-            <Typography color="error.dark" sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
-              <ArrowDown size={16} style={{ transform: 'rotate(45deg)' }} /> 30.6%
-            </Typography>
-          }
-        >
-          <EcommerceDataChart color={theme.palette.error.dark} />
-        </EcommerceDataCard>
-      </Grid> */}
-
-      {/* row 3 */}
-      <Grid item xs={12} md={6} lg={6}>
-        <RankingUsers />
-      </Grid>
-      <Grid item xs={12} md={6} lg={6}>
-        <RankingBooks />
-      </Grid>
+      {[1, 2, 3].includes(user?.categoria) && (
+        <Grid item xs={12} md={6} lg={6}>
+          <RankingUsers />
+        </Grid>
+      )}
+      {[1, 2, 3].includes(user?.categoria) && (
+        <Grid item xs={12} md={6} lg={6}>
+          <RankingBooks />
+        </Grid>
+      )}
     </Grid>
   );
 }
--- src/sections/widget/data/RankingUsers.jsx
+++ src/sections/widget/data/RankingUsers.jsx
@@ -19,7 +19,6 @@ import { flexRender, useReactTable, getCoreRowModel, getFilteredRowModel, getPag
 
 // project import
 import ScrollX from '../../../components/ScrollX';
-import useAuth from '../../../hooks/useAuth';
 import MainCard from '../../../components/MainCard';
 import EmptyReactTable from '../../../pages/tables/react-table/empty';
 import Avatar from '../../../components/@extended/Avatar';
@@ -34,7 +33,6 @@ import { Star1 } from 'iconsax-react';
 function ReactTable({ columns, data, title }) {
   const theme = useTheme();
   const matchDownSM = useMediaQuery(theme.breakpoints.down('sm'));
-  const { user } = useAuth();
   const [columnFilters, setColumnFilters] = useState([]);
   const [rowSelection, setRowSelection] = useState({});
   const [globalFilter, setGlobalFilter] = useState('');
@@ -66,96 +64,94 @@ function ReactTable({ columns, data, title }) {
   );
 
   return (
-    [1, 2, 3].includes(user?.categoria) && data.length > 0 && (
-      <MainCard content={false} title={title}>
-        <Box sx={{ p: 3, pb: 0 }}>
-          <Stack direction="row" alignItems="center" justifyContent="space-between" spacing={1}>
-            <Typography variant="h5">Ranking de Usuarios</Typography>
-            <DebouncedInput
+    <MainCard content={false} title={title}>
+      <Box sx={{ p: 3, pb: 0 }}>
+        <Stack direction="row" alignItems="center" justifyContent="space-between" spacing={1}>
+          <Typography variant="h5">Ranking de Usuarios</Typography>
+          <DebouncedInput
             value={globalFilter ?? ''}
             onFilterChange={(value) => setGlobalFilter(String(value))}
             placeholder={`Buscar en ${data.length} registros...`}
           />
-          </Stack>
-        </Box>
-        <Stack
-          direction={matchDownSM ? 'column' : 'row'}
-          spacing={2}
-          justifyContent="space-between"
-          alignItems="center"
-          sx={{ padding: 2.5 }}
-        >
-
-          <Stack direction={matchDownSM ? 'column' : 'row'} alignItems="center" spacing={2}>
-            {/* <SelectColumnSorting {...{ getState: table.getState, getAllColumns: table.getAllColumns }} /> */}
-          </Stack>
         </Stack>
-        <ScrollX>
-          <Stack>
-            <TableContainer>
-              <Table>
-                <TableHead>
-                  {table.getHeaderGroups().map((headerGroup) => (
-                    <TableRow key={headerGroup.id}>
-                      {headerGroup.headers.map((header) => {
-                        if (header.column.columnDef.meta !== undefined && header.column.getCanSort()) {
-                          Object.assign(header.column.columnDef.meta, {
-                            className: header.column.columnDef.meta.className + ' cursor-pointer prevent-select'
-                          });
-                        }
+      </Box>
+      <Stack
+        direction={matchDownSM ? 'column' : 'row'}
+        spacing={2}
+        justifyContent="space-between"
+        alignItems="center"
+        sx={{ padding: 2.5 }}
+      >
 
-                        return (
-                          <TableCell
-                            key={header.id}
-                            {...header.column.columnDef.meta}
-                            onClick={header.column.getToggleSortingHandler()}
-                            {...(header.column.getCanSort() &&
-                              header.column.columnDef.meta === undefined && {
-                                className: 'cursor-pointer prevent-select'
-                              })}
-                          >
-                            {header.isPlaceholder ? null : (
-                              <Stack direction="row" spacing={1} alignItems="center">
-                                <Box>{flexRender(header.column.columnDef.header, header.getContext())}</Box>
-                                {header.column.getCanSort() && <HeaderSort column={header.column} />}
-                              </Stack>
-                            )}
-                          </TableCell>
-                        );
-                      })}
-                    </TableRow>
-                  ))}
-                </TableHead>
-                <TableBody>
-                  {table.getRowModel().rows.map((row) => (
-                    <TableRow key={row.id}>
-                      {row.getVisibleCells().map((cell) => (
-                        <TableCell key={cell.id} {...cell.column.columnDef.meta}>
-                          {flexRender(cell.column.columnDef.cell, cell.getContext())}
+        <Stack direction={matchDownSM ? 'column' : 'row'} alignItems="center" spacing={2}>
+          {/* <SelectColumnSorting {...{ getState: table.getState, getAllColumns: table.getAllColumns }} /> */}
+        </Stack>
+      </Stack>
+      <ScrollX>
+        <Stack>
+          <TableContainer>
+            <Table>
+              <TableHead>
+                {table.getHeaderGroups().map((headerGroup) => (
+                  <TableRow key={headerGroup.id}>
+                    {headerGroup.headers.map((header) => {
+                      if (header.column.columnDef.meta !== undefined && header.column.getCanSort()) {
+                        Object.assign(header.column.columnDef.meta, {
+                          className: header.column.columnDef.meta.className + ' cursor-pointer prevent-select'
+                        });
+                      }
+
+                      return (
+                        <TableCell
+                          key={header.id}
+                          {...header.column.columnDef.meta}
+                          onClick={header.column.getToggleSortingHandler()}
+                          {...(header.column.getCanSort() &&
+                            header.column.columnDef.meta === undefined && {
+                            className: 'cursor-pointer prevent-select'
+                          })}
+                        >
+                          {header.isPlaceholder ? null : (
+                            <Stack direction="row" spacing={1} alignItems="center">
+                              <Box>{flexRender(header.column.columnDef.header, header.getContext())}</Box>
+                              {header.column.getCanSort() && <HeaderSort column={header.column} />}
+                            </Stack>
+                          )}
                         </TableCell>
-                      ))}
-                    </TableRow>
-                  ))}
-                  <TableRow sx={{ '&:hover': { bgcolor: 'transparent !important' } }}>
-                    <TableCell sx={{ p: 2, py: 3 }} colSpan={9}>
-                      <TablePagination
-                        {...{
-                          setPageSize: table.setPageSize,
-                          setPageIndex: table.setPageIndex,
-                          getState: table.getState,
-                          getPageCount: table.getPageCount,
-                          initialPageSize: 4
-                        }}
-                      />
-                    </TableCell>
+                      );
+                    })}
                   </TableRow>
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </Stack>
-        </ScrollX>
-      </MainCard>
-    )
+                ))}
+              </TableHead>
+              <TableBody>
+                {table.getRowModel().rows.map((row) => (
+                  <TableRow key={row.id}>
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id} {...cell.column.columnDef.meta}>
+                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))}
+                <TableRow sx={{ '&:hover': { bgcolor: 'transparent !important' } }}>
+                  <TableCell sx={{ p: 2, py: 3 }} colSpan={9}>
+                    <TablePagination
+                      {...{
+                        setPageSize: table.setPageSize,
+                        setPageIndex: table.setPageIndex,
+                        getState: table.getState,
+                        getPageCount: table.getPageCount,
+                        initialPageSize: 4
+                      }}
+                    />
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Stack>
+      </ScrollX>
+    </MainCard>
   );
 }
 
@@ -182,10 +178,10 @@ export default function RankingUsers() {
                 alt="Avatar 1"
                 size="lg"
                 variant="rounded"
-                
+
               >
                 {row.original.nombres.charAt(0)}
-                </Avatar>
+              </Avatar>
               <Stack spacing={0}>
                 <Typography variant="subtitle1">{row.original.nombres}</Typography>
               </Stack>
@@ -212,7 +208,7 @@ export default function RankingUsers() {
   return (
     <MainCard content={false}>
       <ScrollX>
-      {rankingUsersLoading ? <EmptyReactTable /> : <ReactTable {...{ data: list, columns, modalToggler: () => {} }} />}
+        {rankingUsersLoading ? <EmptyReactTable /> : <ReactTable {...{ data: list, columns, modalToggler: () => { } }} />}
       </ScrollX>
     </MainCard>
   );
