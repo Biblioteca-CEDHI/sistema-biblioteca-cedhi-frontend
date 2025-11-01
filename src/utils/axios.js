27c2597fa16096117a91238454e5fec217d4b9cd
AngelesVasquezU <maria.vasquez.pineda@ucsp.edu.pe>
2025-11-01 01:23:38 -0500

    logs

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+console.log("AXIOS BASE URL:", import.meta.env.VITE_APP_API_URL);
 
 const axiosServices = axios.create({ baseURL: import.meta.env.VITE_APP_API_URL || 'https://sistema-biblioteca-cedhi-backend.onrender.com' });
 
