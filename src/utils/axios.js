d72158c9f642f78f19303e23dfe96b2c965d21d8
AngelesVasquezU <maria.vasquez.pineda@ucsp.edu.pe>
2025-11-09 22:40:41 -0500

    urls pruebas

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const axiosServices = axios.create({ baseURL: import.meta.env.VITE_APP_API_URL || 'https://sistema-biblioteca-cedhi-backend.onrender.com' });
+const axiosServices = axios.create({ baseURL: import.meta.env.VITE_APP_API_URL || 'http://api.salalectura.cedhinuevaarequipa.edu.pe' });
 
 // ==============================|| AXIOS - FOR MOCK SERVICES ||============================== //
 
